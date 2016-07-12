2e3c0472b55e6082c1fd7b2294b793124cca73c6
thatmose <moseswanyonyi@gmail.com>
2016-07-11 18:07:11 -0600

    migration

---
+++ db/migrate/20160711235856_songs.rb
@@ -0,0 +1,10 @@
+class Songs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :author
+      t.string :url
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160711235856) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
